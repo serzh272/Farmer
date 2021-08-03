d8f0bee88c29e80466e0d8de406afa4907d79283
serzh272 <serzh272@mail.ru>
2021-08-03 22:24:04 +0900

    Remove DatabaseManager

--- app/src/main/java/ru/serzh272/farmer/data/local/AppDb.kt
+++ app/src/main/java/ru/serzh272/farmer/data/local/AppDb.kt
@@ -1,22 +1,14 @@
 package ru.serzh272.farmer.data.local
 
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
-import ru.serzh272.farmer.App
 import ru.serzh272.farmer.BuildConfig
-import ru.serzh272.farmer.data.local.AppDb.Companion.DATABASE_NAME
 import ru.serzh272.farmer.data.local.converters.FieldConverter
 import ru.serzh272.farmer.data.local.dao.FieldDao
 import ru.serzh272.farmer.data.local.entities.Field
 
-object DbManager {
-val db = Room.databaseBuilder(
-    App.applicationContext(), AppDb::class.java, DATABASE_NAME
-)
-    .build()
-}
+
 @Database(
     entities = [Field::class],
     version = AppDb.DATABASE_VERSION,
